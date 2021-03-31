47086af0049d22f2b193b6f47013cc24d54ab60a
CarlosEduardo66 <79456846+CarlosEduardo66@users.noreply.github.com>
2021-03-30 21:09:39 -0300

    Create doacao.php

---
+++ doacao.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doação de sangue</title>
+</head>
+<body>
+<h1>Doação de Sangue</h1>
+    <form method="GET" action="info_sangue.php">
+    <input type="text" name="suaidade" placeholder="Qual a sua idade?">
+    <input type="text" name="seupeso" placeholder="Qual o seu peso?">
+    <br></br>
+    <input type="submit" value="Verificar">
+    </form>
+</body>
+</html>
